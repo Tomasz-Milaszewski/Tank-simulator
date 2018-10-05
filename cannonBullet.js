5ef05169bf80cad439dbbfc5fd795e5a8def755d
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-05 21:22:20 +0200

    WIP: shooting bullets

--- cannonBullet.js
+++ cannonBullet.js
@@ -5,7 +5,7 @@ function cannonBullet() {
         if (event.code === 'Space') {
             if (spaceCounter % 2 === 0) {
                 bulletLoading();
-                spaceCounter +=1;
+                spaceCounter += 1;
             }
         }
     });
@@ -13,17 +13,52 @@ function cannonBullet() {
 cannonBullet();
 
 var loadingInterval;
+var bulletLoaded = false;
+
 function bulletLoading() {
     var loadingBar = document.createElement('div');
     loadingBar.classList.add('loadingBar');
     app.appendChild(loadingBar);
 
     var leftBorderThickness = 0;
-    loadingInterval = setInterval(function() {
+    loadingInterval = setInterval(function () {
         leftBorderThickness += 2;
         loadingBar.style.borderLeftWidth = leftBorderThickness + 'px';
     }, 50);
     setTimeout(function () {
         clearInterval(loadingInterval);
+        bulletLoaded = true;
     }, 3000);
+}
+
+function trigger() {
+    document.addEventListener("keydown", function (event) {
+        if (event.code === 'Space') {
+            if (spaceCounter % 2 !== 0 && bulletLoaded === true) {
+                shot();
+                spaceCounter = 0;
+            } else {return;}
+        }
+    })
+}
+
+function shot() {
+    //bullet creation
+    var cannon = document.querySelector('.towerCannon');
+    var bullet = document.createElement('div');
+    bullet.classList.add('bullet');
+    cannon.appendChild(bullet);
+
+    //bullet movement
+    // var canonAngleDegree = getDegree(tank);
+    // var canonAngleRadians = canonAngleDegree * (Math.PI / 180);
+
+    // loadingInterval = setInterval(function () {
+    //     leftBorderThickness += 2;
+    //     loadingBar.style.borderLeftWidth = leftBorderThickness + 'px';
+    // }, 50);
+    // setTimeout(function () {
+    //     clearInterval(loadingInterval);
+    //     bulletLoaded = true;
+    // }, 3000);
 }
