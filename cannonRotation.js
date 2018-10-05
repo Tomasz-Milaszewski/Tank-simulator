d239243f3f77d08a3e155b35aa06858f2dcba87a
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-05 14:31:51 +0200

    adds cannon rotation

--- cannonRotation.js
+++ cannonRotation.js
@@ -6,18 +6,23 @@ function cannonRotation() {
     function myFunction(e) {
         var x = e.clientX;
         var y = e.clientY;
-                
+        
         var cannon = document.querySelector('.towerCannon');
         var cannonCenterX = (cannon.getBoundingClientRect().left + cannon.getBoundingClientRect().right)/2; 
-        var cannonCenterY = (cannon.getBoundingClientRect().top + cannon.getBoundingClientRect().bottom)/2;
         
+        var cannonCenterY = (cannon.getBoundingClientRect().top + cannon.getBoundingClientRect().bottom)/2;
         var deltaX = x - cannonCenterX;
         var deltaY = y - cannonCenterY;
-        
-        var cannonAngleRadians = Math.atan(deltaY/deltaX);
-        var cannonAngleDegrees = cannonAngleRadians * (180 / Math.PI);
-        console.log(cannonAngleDegrees);
-        cannon.style.transform = 'rotate(' + cannonAngleDegrees + 'deg)';
+
+        if (deltaX > 0) {
+            var cannonAngleRadians = Math.atan(deltaY/deltaX);
+            var cannonAngleDegrees = cannonAngleRadians * (180 / Math.PI);
+            cannon.style.transform = 'rotate(' + cannonAngleDegrees + 'deg)';
+        } else {
+            var cannonAngleRadians = Math.atan(deltaY/deltaX);
+            var cannonAngleDegrees = cannonAngleRadians * (180 / Math.PI) + 180;
+            cannon.style.transform = 'rotate(' + cannonAngleDegrees + 'deg)';
+        }
     }
 }
 cannonRotation();
