e54649dff31e47f89b50c5e1d144290759d7fdef
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-05 09:35:01 +0200

    adds getDegree function to access current objects rotations

--- tank.js
+++ tank.js
@@ -1,5 +1,5 @@
+//generates tank and sends it to battlefield
 const app = document.querySelector('.tankContainer');
-
 const tank = document.createElement('div');
 const tankTower = document.createElement('div');
 const towerCannon = document.createElement('div');
@@ -9,5 +9,85 @@ towerCannon.classList.add('towerCannon');
 tank.appendChild(towerCannon);
 tankTower.classList.add('tankTower');
 tank.appendChild(tankTower);
-
 app.appendChild(tank);
+
+//tank movement
+// console.log(tank.getBoundingClientRect());
+// console.log(tank.getBoundingClientRect().top);
+// console.log(tank.getBoundingClientRect().right);
+// console.log(tank.getBoundingClientRect().bottom);
+// console.log(tank.getBoundingClientRect().left);
+
+const tankVelocity = 0.1;
+const tankRotationVelocity = 0.1;
+var isWPressed = false;
+var isSPressed = false;
+var isAPressed = false;
+var isDPressed = false;
+var leftOffset = 0;
+var topOffset = 0;
+var rotationDegree = 0;
+var time = 10;
+
+getDegree(tank);
+
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'LeftArrow') {
+        isWPressed = true
+        console.log('true');
+    }
+})
+window.addEventListener('keyup', function () {
+    if (event.code === 'W') {
+        isWPressed = false;
+    }
+})
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'S') {
+        isSPressed = true;
+    }
+})
+window.addEventListener('keyup', function () {
+    if (event.code === 'S') {
+        isSPressed = false;
+    }
+})
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'A') {
+        isAPressed = true;
+    }
+})
+window.addEventListener('keyup', function () {
+    if (event.code === 'A') {
+        isAPressed = false;
+    }
+})
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'D') {
+        isDPressed = true;
+    }
+})
+window.addEventListener('keyup', function () {
+    if (event.code === 'D') {
+        isDPressed = false;
+    }
+})
+
+// setInterval(function () {
+//     if (letsPlayLock) { return; }
+//     if (isLeftArrowPressed && leftOffset >= 1) {
+//         velocity = basketVelocity;
+//         leftOffset = leftOffset - basketVelocity * basketTime;
+//         basket.style.left = leftOffset + 'px'
+//     }
+//     //right move includes limiting condition for wider basket - limitation from the right
+//     if (isRightArrowPressed && leftOffset <= 449 && (leftOffset + basket.getBoundingClientRect().width) < 500) {
+//         velocity = basketVelocity;
+//         leftOffset = leftOffset + basketVelocity * basketTime;
+//         basket.style.left = leftOffset + 'px'
+//     } else {
+//         velocity = 0;
+//     }
+//     PageTransitionEvent
+// }, basketTime)
+
