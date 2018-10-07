9a6ed05ab6e12b5fcb72c63318a97b34e08a4415
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-07 13:14:22 +0200

    solves bullet positioning at teh end of cannon

--- setBulletPosition.js
+++ setBulletPosition.js
@@ -1,15 +1,11 @@
 function setBulletPosition() {
     const cannon = document.querySelector('.towerCannon');
-    const cannonRect = cannon.getBoundingClientRect();
-    console.log(cannonRect);
+    // const cannonRect = cannon.getBoundingClientRect();
+    // console.log(cannonRect);
     const cannonLeft = cannon.getBoundingClientRect().left;
-    const cannonRight = cannon.getBoundingClientRect().right;
-    const cannonDeltaLeft = Math.abs(cannonRight - cannonLeft);
     const cannonTop = cannon.getBoundingClientRect().top;
-    const cannonBottom = cannon.getBoundingClientRect().bottom;
-    const cannonDeltaTop = Math.abs(cannonBottom - cannonTop);
     const cannonAngleDegree = getDegree(cannon);
-    console.log(cannonAngleDegree);
+    // console.log(cannonAngleDegree);
     const cannonAngleRadians = cannonAngleDegree * (Math.PI / 180);
 
     if (cannonAngleDegree >= 0 && cannonAngleDegree <=90) {
@@ -24,6 +20,8 @@ function setBulletPosition() {
         const bulletLeftTop = [cannonLeft + Math.abs(8.5 * Math.cos(cannonAngleRadians)), cannonTop + Math.abs(8.5 * Math.sin(cannonAngleRadians))];
         return bulletLeftTop;
     }
-    
-    
+    if (cannonAngleDegree > -90 && cannonAngleDegree < 0) {
+        const bulletLeftTop = [cannonLeft + Math.abs(50 * Math.cos(cannonAngleRadians)), cannonTop + Math.abs(8.5 * Math.sin(cannonAngleRadians))];
+        return bulletLeftTop;
+    }
 }
